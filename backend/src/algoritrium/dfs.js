ab735de7e14bdbc38fab596f4393a85d051cb924
anubhavsingh2027 <anubhavsingh2027@gmail.com>
2026-01-20 08:03:22 +0530

    add dfs code in cpp

--- backend/src/algoritrium/dfs.js
+++ backend/src/algoritrium/dfs.js
@@ -206,3 +206,167 @@ console.log("Connected Components:", graph.getConnectedComponents());
 module.exports = Graph;
 
 `;
+
+exports.dfsCpp=`
+// Depth-First Search (DFS) Algorithm
+// DFS explores as far as possible along each branch before backtracking, using a stack or recursion
+
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Graph {
+private:
+  map<char, vector<char>> adjacencyList;
+
+  bool dfsHasCycle(char vertex, set<char>& visited, set<char>& recursionStack) {
+    visited.insert(vertex);
+    recursionStack.insert(vertex);
+
+    for (char neighbor : adjacencyList[vertex]) {
+      if (visited.find(neighbor) == visited.end()) {
+        if (dfsHasCycle(neighbor, visited, recursionStack)) {
+          return true;
+        }
+      } else if (recursionStack.find(neighbor) != recursionStack.end()) {
+        return true;
+      }
+    }
+
+    recursionStack.erase(vertex);
+    return false;
+  }
+
+  void dfsComponent(char vertex, set<char>& visited, vector<char>& component) {
+    visited.insert(vertex);
+    component.push_back(vertex);
+
+    for (char neighbor : adjacencyList[vertex]) {
+      if (visited.find(neighbor) == visited.end()) {
+        dfsComponent(neighbor, visited, component);
+      }
+    }
+  }
+
+public:
+  void addVertex(char vertex) {
+    if (adjacencyList.find(vertex) == adjacencyList.end()) {
+      adjacencyList[vertex] = vector<char>();
+    }
+  }
+
+  void addEdge(char vertex1, char vertex2) {
+    addVertex(vertex1);
+    addVertex(vertex2);
+    adjacencyList[vertex1].push_back(vertex2);
+    adjacencyList[vertex2].push_back(vertex1);
+  }
+
+  vector<char> dfsRecursive(char startVertex, set<char>& visited, vector<char>& result) {
+    visited.insert(startVertex);
+    result.push_back(startVertex);
+
+    for (char neighbor : adjacencyList[startVertex]) {
+      if (visited.find(neighbor) == visited.end()) {
+        dfsRecursive(neighbor, visited, result);
+      }
+    }
+
+    return result;
+  }
+
+  vector<char> dfsIterative(char startVertex) {
+    set<char> visited;
+    stack<char> st;
+    vector<char> result;
+
+    visited.insert(startVertex);
+    st.push(startVertex);
+
+    while (!st.empty()) {
+      char vertex = st.top();
+      st.pop();
+      result.push_back(vertex);
+
+      for (char neighbor : adjacencyList[vertex]) {
+        if (visited.find(neighbor) == visited.end()) {
+          visited.insert(neighbor);
+          st.push(neighbor);
+        }
+      }
+    }
+
+    return result;
+  }
+
+  bool hasCycle() {
+    set<char> visited, recursionStack;
+
+    for (auto& pair : adjacencyList) {
+      if (visited.find(pair.first) == visited.end()) {
+        if (dfsHasCycle(pair.first, visited, recursionStack)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  vector<vector<char>> getConnectedComponents() {
+    set<char> visited;
+    vector<vector<char>> components;
+
+    for (auto& pair : adjacencyList) {
+      if (visited.find(pair.first) == visited.end()) {
+        vector<char> component;
+        dfsComponent(pair.first, visited, component);
+        components.push_back(component);
+      }
+    }
+
+    return components;
+  }
+};
+
+int main() {
+  Graph graph;
+
+  graph.addEdge('A', 'B');
+  graph.addEdge('A', 'C');
+  graph.addEdge('B', 'D');
+  graph.addEdge('C', 'E');
+  graph.addEdge('D', 'E');
+  graph.addEdge('E', 'F');
+
+  set<char> visited;
+  vector<char> result;
+  cout << "DFS (Recursive) from A: ";
+  for (char c : graph.dfsRecursive('A', visited, result)) {
+    cout << c << " ";
+  }
+  cout << endl;
+
+  cout << "DFS (Iterative) from A: ";
+  for (char c : graph.dfsIterative('A')) {
+    cout << c << " ";
+  }
+  cout << endl;
+
+  cout << "Has Cycle: " << (graph.hasCycle() ? "true" : "false") << endl;
+
+  cout << "Connected Components: ";
+  for (auto& component : graph.getConnectedComponents()) {
+    for (char c : component) {
+      cout << c << " ";
+    }
+    cout << "| ";
+  }
+  cout << endl;
+
+  return 0;
+}
+`;
