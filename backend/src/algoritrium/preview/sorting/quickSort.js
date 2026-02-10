8d736b1bf75e24e9ad78852ce29d26b4688a4398
anubhavsingh2027 <anubhavsingh2027@gmail.com>
2026-02-10 07:38:29 +0530

    add new program

---
+++ backend/src/algoritrium/preview/sorting/quickSort.js
@@ -0,0 +1,54 @@
+exports.quickSortAlgo = `
+function quickSort(arr, low = 0, high = arr.length - 1) {
+  if (low < high) {
+    const pi = partition(arr, low, high);
+    quickSort(arr, low, pi - 1);
+    quickSort(arr, pi + 1, high);
+  }
+  return arr;
+}
+
+function partition(arr, low, high) {
+  const pivot = arr[high];
+  let i = low - 1;
+
+  for (let j = low; j < high; j++) {
+    if (arr[j] < pivot) {
+      i++;
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  }
+
+  [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
+  return i + 1;
+}
+`;
+
+exports.quickSortCpp = `
+#include <vector>
+#include <algorithm>
+
+int partition(std::vector<int>& arr, int low, int high) {
+  int pivot = arr[high];
+  int i = low - 1;
+
+  for (int j = low; j < high; j++) {
+    if (arr[j] < pivot) {
+      i++;
+      std::swap(arr[i], arr[j]);
+    }
+  }
+
+  std::swap(arr[i + 1], arr[high]);
+  return i + 1;
+}
+
+void quickSort(std::vector<int>& arr, int low, int high) {
+  if (low < high) {
+    int pi = partition(arr, low, high);
+    quickSort(arr, low, pi - 1);
+    quickSort(arr, pi + 1, high);
+  }
+}
+`;
+
