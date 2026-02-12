cd5c3fe0461fa8cb2223ba3903ff92c63b8e489e
anubhavsingh2027 <anubhavsingh2027@gmail.com>
2026-02-12 09:10:31 +0530

    new code add

--- backend/src/algoritrium/preview/sorting/quickSort.js
+++ backend/src/algoritrium/preview/sorting/quickSort.js
@@ -52,3 +52,88 @@ void quickSort(std::vector<int>& arr, int low, int high) {
 }
 `;
 
+exports.quickSortJava = `
+import java.util.*;
+
+public class QuickSort {
+  public static int partition(List<Integer> arr, int low, int high) {
+    int pivot = arr.get(high);
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+      if (arr.get(j) < pivot) {
+        i++;
+        Collections.swap(arr, i, j);
+      }
+    }
+
+    Collections.swap(arr, i + 1, high);
+    return i + 1;
+  }
+
+  public static void quickSort(List<Integer> arr, int low, int high) {
+    if (low < high) {
+      int pi = partition(arr, low, high);
+      quickSort(arr, low, pi - 1);
+      quickSort(arr, pi + 1, high);
+    }
+  }
+
+  public static void quickSort(List<Integer> arr) {
+    if (arr.size() > 0) {
+      quickSort(arr, 0, arr.size() - 1);
+    }
+  }
+}
+`;
+
+exports.quickSortJavaScript = `
+function quickSort(arr, low = 0, high = arr.length - 1) {
+  if (low < high) {
+    const pi = partition(arr, low, high);
+    quickSort(arr, low, pi - 1);
+    quickSort(arr, pi + 1, high);
+  }
+  return arr;
+}
+
+function partition(arr, low, high) {
+  const pivot = arr[high];
+  let i = low - 1;
+
+  for (let j = low; j < high; j++) {
+    if (arr[j] < pivot) {
+      i++;
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  }
+
+  [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
+  return i + 1;
+}
+`;
+
+exports.quickSortPython = `
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+def quickSort(arr, low=0, high=None):
+    if high is None:
+        high = len(arr) - 1
+
+    if low < high:
+        pi = partition(arr, low, high)
+        quickSort(arr, low, pi - 1)
+        quickSort(arr, pi + 1, high)
+
+    return arr
+`;
