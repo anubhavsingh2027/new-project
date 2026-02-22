d07933eae1f1f571a1ca695628eb8e0e52cb0264
anubhavsingh2027 <anubhavsingh2027@gmail.com>
2026-02-22 10:54:41 +0530

    update

--- backend/src/algoritrium/preview/sorting/heapSort.js
+++ backend/src/algoritrium/preview/sorting/heapSort.js
@@ -132,4 +132,35 @@ function heapify(arr, n, i) {
 heapSort([64, 34, 25, 12, 22, 11, 90]);
 `;
 
+exports.heapSortPhython = `
+def heapSort(arr):
+    n = len(arr)
 
+    # Build max heap
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    # Extract elements one by one from heap
+    for i in range(n - 1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        heapify(arr, i, 0)
+
+    return arr
+
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+heapSort([64, 34, 25, 12, 22, 11, 90])
+`;
